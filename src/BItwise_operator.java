a280187cbf1d4c9a435f98ba8b5fc6379449668b
Tejas Lale <tejaslale29@gmail.com>
2024-01-15 18:58:50 +0530

    Initial commit

---
+++ src/BItwise_operator.java
@@ -0,0 +1,16 @@
+public class BItwise_operator {
+
+    public static void main(String[] args) {
+        int p= 2,q=33;
+
+        System.out.println(p|q);
+        System.out.println(p & q);
+        System.out.println(p ^ q);
+
+        System.out.println(p<<1);
+        System.out.println(p>>1);
+
+        System.out.println(q>>1);
+        System.out.println(q>>2);
+    }
+}
